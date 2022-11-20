874d52a54e46357076851355c88863bc4336335f
Marcel Müller <neikos@neikos.email>
2022-11-20 21:40:20 +0100

    Add remember option
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,6 @@
 class UserSessionsController < ApplicationController
   def create
-    @user = login(params[:email], params[:password])
+    @user = login(params[:email], params[:password], params[:remember])
 
     if @user
       redirect_back_or_to(:users, notice: 'Login successful')
