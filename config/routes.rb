0fb0d8d2df4645f552a721afea2a0317e1b66b1c
Marcel Müller <neikos@neikos.email>
2022-11-20 21:36:39 +0100

    Add user + sessions
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "index#root"
+  resources :users
+
+  get 'login' => 'user_sessions#new', as: :login
+  post 'login' => 'user_sessions#create'
+  post 'logout' => 'user_sessions#destroy', as: :logout
 end
