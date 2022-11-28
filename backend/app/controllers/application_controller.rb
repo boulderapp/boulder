f65bc3300529afb82ce7ba5f238ae211f51e6e36
Marcel Müller <neikos@neikos.email>
2022-11-28 18:23:19 +0100

    Switch to rails API
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include ::ActionController::Cookies
+
   before_action :require_login
 
   private
