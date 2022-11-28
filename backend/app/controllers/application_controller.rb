b46e883467ad9082599abd62b4f50ae64b6f5592
Marcel Müller <neikos@neikos.email>
2022-11-28 18:23:41 +0100

    Add a workaround for sorcery limitation
    Currently sorcery doesn't neatly support rails API.
    
    This is tracked in https://github.com/Sorcery/sorcery/issues/330
    
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::API
   before_action :require_login
 
   private
+
+  def form_authenticity_token
+  end
+
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
   end
